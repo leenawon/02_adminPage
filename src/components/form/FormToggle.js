d54dfbf9b40c8ec808243bcbe3e16e297450d7d2
leenawon <nawon5154@gmail.com>
2022-03-29 23:43:05 +0900

    Modify - FormToggle 컴포넌트 스타일 theme 색상 사용하도록 수정

--- src/components/form/FormToggle.js
+++ src/components/form/FormToggle.js
@@ -20,15 +20,25 @@ const ToggleCircle = styled.div`
   width: 22px;
   height: 22px;
   border-radius: 50%;
-  border: 1px solid ${(props) => (props.isClick ? '#2A18BD;' : 'gray;')};
-  background: ${(props) => (props.isClick ? '#2A18BD;' : 'white;')};
+  border: 1px solid
+    ${(props) =>
+      props.isClick
+        ? `${({ theme }) => theme.powerBlue}`
+        : `${({ theme }) => theme.veryLightGrayColor}`};
+  background: ${(props) =>
+    props.isClick
+      ? `${({ theme }) => theme.powerBlue}`
+      : `${({ theme }) => theme.mainBgColor}`};
 `;
 
 const ToggleBackground = styled.div`
   width: 35px;
   height: 15px;
   border-radius: 100px;
-  background: ${(props) => (props.isClick ? '#AEA8E6;' : '#9F9F9F;')};
+  background: ${(props) =>
+    props.isClick
+      ? `${({ theme }) => theme.weekBlue}`
+      : `${({ theme }) => theme.darkGrayColor}`};
 `;
 
 export default function FormToggle({ isClick, setIsClick }) {
